2d8d49638e792d0d159bb7f97b0919633eab8c42
Eddy <eddy@gocollo.com>
2024-10-31 16:25:45 +0900

    add cart and orders

--- src/app/(shop)/orders/index.tsx
+++ src/app/(shop)/orders/index.tsx
@@ -1,34 +1,109 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Link, Stack } from "expo-router";
+import { format } from "date-fns";
 
-export default function Orders() {
+import { Tables } from "../../../types/database.types";
+import { ORDERS } from "../../../../assets/orders";
+
+const renderItem: ListRenderItem<Tables<"order">> = ({ item }) => (
+  <Link href={`/orders/${item.slug}`} asChild>
+    <Pressable style={styles.orderContainer}>
+      <View style={styles.orderContent}>
+        <View style={styles.orderDetailsContainer}>
+          <Text style={styles.orderItem}>{item.slug}</Text>
+          <Text style={styles.orderDetails}>{item.description}</Text>
+          <Text style={styles.orderDate}>
+            {/* {format(new Date(item.created_at), "MMM dd, yyyy")} */}
+          </Text>
+        </View>
+        <View
+          style={[styles.statusBadge, styles[`statusBadge_${item.status}`]]}
+        >
+          <Text style={styles.statusText}>{item.status.toUpperCase()}</Text>
+        </View>
+      </View>
+    </Pressable>
+  </Link>
+);
+
+const Orders = () => {
+  const orders = ORDERS;
+
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Orders</Text>
-        <Text style={styles.subtitle}>Let's get started</Text>
-      </View>
+      <Stack.Screen options={{ title: "Orders" }} />
+      <FlatList
+        data={orders}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export default Orders;
+
+const styles: { [key: string]: any } = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
+  },
+  orderContainer: {
+    backgroundColor: "#f8f8f8",
+    padding: 16,
+    marginVertical: 8,
+    borderRadius: 8,
+  },
+  orderContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    padding: 24,
   },
-  main: {
+  orderDetailsContainer: {
     flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
   },
-  title: {
-    fontSize: 64,
+  orderItem: {
+    fontSize: 18,
     fontWeight: "bold",
   },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+  orderDetails: {
+    fontSize: 14,
+    color: "#555",
+  },
+  orderDate: {
+    fontSize: 12,
+    color: "#888",
+    marginTop: 4,
+  },
+  statusBadge: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    alignSelf: "flex-start",
+  },
+  statusText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  statusBadge_Pending: {
+    backgroundColor: "#ffcc00",
+  },
+  statusBadge_Completed: {
+    backgroundColor: "#4caf50",
+  },
+  statusBadge_Shipped: {
+    backgroundColor: "#2196f3",
+  },
+  statusBadge_InTransit: {
+    backgroundColor: "#ff9800",
   },
 });
